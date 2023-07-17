a26ea02c297bf3cb9779ee142f8f914f05623298
Andres <andrese-rios@utp.edu.co>
2023-07-17 16:34:22 -0500

    adding a loading msg to inform about the loading progress of the image

--- lib/presentation/widgets/chat/her_msg_bubble.dart
+++ lib/presentation/widgets/chat/her_msg_bubble.dart
@@ -55,6 +55,17 @@ class _ImageBubble extends StatelessWidget {
         height: 150,
         width: size.width * 0.7,
         fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if(loadingProgress == null) return child;
+          return Container(
+            width: size.width * 0.7,
+            height: 150,
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            child: Center(
+              child: const Text('Reachel esta enviando una imagen')
+              ),
+          );
+        },
       ),      
       );
   }
