0ac0b306637c51977acd4aa48484de56ad1ab26f
Andres <andrese-rios@utp.edu.co>
2023-07-17 15:48:05 -0500

    taking color from context and apllying it to my_msg_bubble widget

---
+++ lib/presentation/widgets/chat/my_message_bubble.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MyMsgBubble extends StatelessWidget {
+  const MyMsgBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+   final ColorScheme colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [        
+        Container(
+          decoration: BoxDecoration(
+            color: colors.primary,
+            borderRadius: BorderRadius.circular(20)
+          ),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+               vertical: 10
+               ),
+            child:  Text(
+              'lorem ipsum',
+              style: TextStyle(
+                color: Colors.white
+              )
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        )
+      ],
+    );
+  }
+}
